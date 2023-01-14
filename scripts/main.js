a55381a40c56655de3d7e6b4d327992173d14559
Pasha Pasha <mail9827@yandex.ru>
2023-01-14 13:52:07 +0300

    close popup by event.target = event.currentTarget

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,35 @@ closePopup.addEventListener('click', toggleClass)
 function toggleClass() {
     popup.classList.toggle('popup_opened');
 }
+
+popup.addEventListener('click', function(event) {
+    // console.log('target: ', event.target)
+    // console.log('currentTarget: ', event.currentTarget)
+    if (event.target === event.currentTarget) {
+        toggleClass()
+    }
+})
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__container');// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__input_name');// Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input_job');// Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
