aceb74d445496f3b4e6ed0ad073d9e129f1c1c83
Pasha Pasha <mail9827@yandex.ru>
2023-01-25 19:06:47 +0300

    feat: add  newCard

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,16 @@ let saveButton = formElement.querySelector('.popup__button-save');
 let nameInput = formElement.querySelector('.popup__input_name');
 let jobInput = formElement.querySelector('.popup__input_job');
 
+let openPopupNC = document.querySelector('.popupNC-open');
+let popupNewCard = document.querySelector('.popupNewCard');
+let closepopupNewCard = document.querySelector('.popupNewCard__button-close');
+
+let newCardformElement = document.querySelector('.popupNewCard__container');
+let createButton = newCardformElement.querySelector('.popupNewCard__button-create');
+let newCardNameInput = newCardformElement.querySelector('.popupNewCard__input_name');
+let newCardLinkInput = newCardformElement.querySelector('.popupNewCard__input_link');
+
+
 // openPopup.addEventListener('click', function() {
 //     popup.classList.add('popup_opened');
 // })
@@ -19,14 +29,15 @@ let jobInput = formElement.querySelector('.popup__input_job');
 //     popup.classList.remove('popup_opened');
 // })
 
+// Включаем/выкл попап редактирования
 openPopup.addEventListener('click', toggleClass)
-
 closePopup.addEventListener('click', toggleClass)
 
 function toggleClass() {
     popup.classList.toggle('popup_opened');
 }
 
+// Закрытие попапа редактирования нажатием на свобоное пространство вокруг попапа
 popup.addEventListener('click', function(event) {
     // console.log('target: ', event.target)
     // console.log('currentTarget: ', event.currentTarget)
@@ -41,24 +52,18 @@ popup.addEventListener('click', function(event) {
 nameInput.value = 'Жак-Ив Кусто';
 jobInput.value = 'Исследователь океана';
 
-
 // Находим форму в DOM
-
 // Находим поля формы в DOM
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function handleFormSubmit (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
     // Получите значение полей jobInput и nameInput из свойства value
     // Выберите элементы, куда должны быть вставлены значения полей
     // Вставьте новые значения с помощью textContent
     editName.textContent = nameInput.value;
     editProf.textContent = jobInput.value;
-
      toggleClass();
-    
 }
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
@@ -105,23 +110,66 @@ function renderCard(card) {
 	htmlElement.querySelector('.place__title').textContent = card.name;
   htmlElement.querySelector('.place__image').src = card.link;
 
-	cardsList.appendChild(htmlElement);
+	cardsList.prepend(htmlElement);
 }
+
+openPopupNC.addEventListener('click', toggleClassNewCard)
+closepopupNewCard.addEventListener('click', toggleClassNewCard)
+
+function toggleClassNewCard() {
+  popupNewCard.classList.toggle('popupNewCard_opened');
+}
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  // const newCardNameInput = newCardformElement.querySelector('.popupNewCard__input_name');
+  // const newCardLinkInput = newCardformElement.querySelector('.popupNewCard__input_link');
+
+  const newcard = { 
+    name: newCardNameInput.value,
+    link: newCardLinkInput.value
+  };
+  renderCard(newcard);
+  
+  newCardNameInput.value = '';
+  newCardLinkInput.value = '';
+
+  toggleClassNewCard();
+}
+
+newCardformElement.addEventListener('submit', handleSubmit);
 
 renderCards();
 
-// 1) добавить 6 карточек из коробки
+// 1) добавить 6 карточек из коробки +
 // 2) Добавьте в проект форму добавления новой карточки. Макет найдёте в «Фигме».
-// Сделайте так, чтобы форма открывалась нажатием на кнопку «+» и закрывалась кликом на крестик
+// Сделайте так, чтобы форма открывалась нажатием на кнопку «+» и
+// закрывалась кликом на крестик +
+
 // 3) Дайте пользователю возможность добавлять карточки:
 // что бы Можно было написать имя карточки и дать ссылку на картинку.
-// Сделайте так, чтобы при клике на «сохранить» новая карточка попадала в начало контейнера с ними.
+// Сделайте так, чтобы при клике на «сохранить» новая карточка попадала 
+// в начало контейнера с ними.
 //  А диалоговое окно после добавления автоматически закрывалось.
 // Чтобы создавать новые карточки, добавьте обработчик событий submit. 
-// Сделайте это аналогично прошлому спринту, в котором вы настраивали редактирование информации о пользователе.
+// Сделайте это аналогично прошлому спринту, в котором вы настраивали 
+// редактирование информации о пользователе.
+
+// -новые значения импут
+// -при клике на сохранить в начало массива добавлялась 
+// новый индекс с новым нейм и линк метод unshift потом опять вызвать функцию
+// function renderCards() чтобы список карточек обновился и новая добавилась?
+// или 
+// в нью попап контенере менялись значения срси и тайтл 
+// и этот обновленый элемент добавлялся препендом в контейнер с классом элементс?
+
+// -при клике на сохранить убирался класс опен с попапа +
+
 // 4) Сделайте так, чтобы карточки можно было лайкать:
 // Если лайкнуть карточку, сердечко поменяет цвет.
 // 5) Добавьте карточкам иконку удаления. 
 // настройте, чтобы карточка удалялась при клике на эту иконку:
-// 6) Настройте просмотр фотографий. Пусть открываются нажатием на картинку и закрываются кликом на крестик:
-// 7) Сделайте так, чтобы все попапы плавно открывались. Пусть проявляются из прозрачности и уходят в неё при закрытии
+// 6) Настройте просмотр фотографий. Пусть открываются нажатием на картинку 
+// и закрываются кликом на крестик:
+// 7) Сделайте так, чтобы все попапы плавно открывались. Пусть проявляются 
+// из прозрачности и уходят в неё при закрытии
